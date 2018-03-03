9a43328899bbd098e55c5f8d4ffd31a0d6499dc4
Koki Tang <kokitangwaichun@gmail.com>
2018-03-04 02:29:33 +0800

    Release 0.1.0

--- Example/KTLoadingView/ViewController.swift
+++ Example/KTLoadingView/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         KTLoadingView.show(in: view, text: "Loading", animateText: "...")
-        KTLoadingView.show(in: view, text: "Hahaha", animateText: ".....", sizeStyle: .center(CGSize.init(width: 150, height: 150)))
     }
 
     override func didReceiveMemoryWarning() {
